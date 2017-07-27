c0c790997dd2cc0ec752f9ea6d9f5993f8ac94ed
Design Daddy <grant@beamery.com>
2017-07-27 21:21:35 +0100

    removed old stuff

--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,18 +1,1 @@
-import smoothScroll from 'smooth-scroll'
-import debounce from 'lodash/debounce'
-
-const SCROLL_SNAP_DEBOUNCE = 800
-
-window.onscroll = debounce(scrollToNearestSection, SCROLL_SNAP_DEBOUNCE)
-
-function distanceFromTop(el) {
-  const bounds = el.getBoundingClientRect()
-  return Math.abs(bounds.top)
-}
-
-function scrollToNearestSection() {
-  const nearestSection = Array.from(document.querySelectorAll('.js-home-section'))
-    .sort((s1, s2) => distanceFromTop(s1) - distanceFromTop(s2))[0]
-
-  smoothScroll.animateScroll(nearestSection)
-}
+console.log("Home");
