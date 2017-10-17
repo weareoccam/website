7e552f9776c7c172a1e6d98ee9a7a16788193d3e
Design Daddy <grant@beamery.com>
2017-10-17 22:58:18 +0100

    adde header scroll animations

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,6 +1,31 @@
+const $ = require ('jquery');
+
 // Mobile nav
 document.querySelector('.js-toggle-nav').addEventListener('click', () => {
   document.querySelector('.js-page').classList.toggle('is-clipped')
   document.querySelector('.js-mobile-nav').classList.toggle('is-open')
   document.querySelector('.js-hamburger').classList.toggle('is-open')
 })
+
+$(window).scroll(function(){
+
+  // Scroll & Window variables
+  var scrollTop = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  var fadeProgress = 1 - (scrollTop / (0.7 * windowHeight));    // Opacity: 0; after scroll down 50% of viewport height
+  var slideProgress = 1 - (windowHeight * (scrollTop / windowHeight));  // Return desired percentage of distance scrolled
+
+  // negative scrolling zero
+  if (scrollTop < 0) scrollTop = 0;
+
+  if (slideProgress > 1) slideProgress = 1;
+
+  // Fade & offset position during scroll event (via xProgress variables)
+  $('.c-top-section__heading').css('opacity', fadeProgress);
+  $('.c-top-section__subheading').css('opacity', fadeProgress);
+
+  $('.c-top-section__heading').css('transform', 'translateY(' +  -0.15 * slideProgress + 'px)');
+  $('.c-top-section__subheading').css('transform', 'translateY(' +  -0.15 * slideProgress + 'px)');
+
+  console.log(scrollTop);
+});
--- source/scripts/pages/home.js
+++ source/scripts/pages/home.js
@@ -1,9 +1,1 @@
-const AOS = require("aos");
-
-// AOS.init({
-// 	duration: 350,
-// 	easing: 'ease-out-cubic',
-//   once: true
-// });
-
 console.log('home');
